277a4e60be1dbefe35a59f9670947fc9cd060a2a
Itachi_Chen <Itachi_Chen@wistron.com>
2021-04-09 17:20:39 +0800

    git init

---
+++ oteldemo/src/test/java/com/example/otel/oteldemo/OteldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otel.oteldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OteldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
